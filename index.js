b4856552811fe240769b981f04a31509e143f748
Maxwell-Robinson <maxwell.robinson@hotmail.com>
2016-09-23 15:56:16 +1200

    all working well

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import ReactDOM, { render } from 'react-dom'
 import App from './client/components/app'
 
 const INITIAL_DATA = {
-  name: "Cameron Shotter",
-  role: "Web Developer & Creator of Awesome Things_"
+  name: "Maxwell Robinson",
+  role: "Web Developer & Creator of Awesome Things"
 }
 
 ReactDOM.render(<App {...INITIAL_DATA} />, document.querySelector('main'))
